5c5aa012332f30b8fefa6f9ee52cb0a9060c4a87
ldhalstrom <loganhalstrom@gmail.com>
2016-10-04 15:36:36 -0700

    added data processing for hw1

---
+++ work/hw1/hw1_00_process.py
@@ -0,0 +1,27 @@
+"""HW1 - DATA PROCESSING
+Logan Halstrom
+MAE 298 AEROACOUSTICS
+HOMEWORK 1 - SIGNAL PROCESSING
+CREATED: 04 OCT 2016
+MODIFIY: 04 OCT 2016
+
+DESCRIPTION: Read sound file of sonic boom and convert signal:
+Narrow-band in Pa
+Single-side power spectral density (FFT)
+1/3 octave and octave band
+"""
+
+import numpy as np
+import os
+
+
+import matplotlib.pyplot as plt
+
+
+def main():
+    """input description
+    """
+
+
+if __name__ == "__main__":
+    main()
---
+++ work/hw1/hw1_01_plot.py
@@ -0,0 +1,32 @@
+"""HW1 - DATA PLOTTING
+Logan Halstrom
+MAE 298 AEROACOUSTICS
+HOMEWORK 1 - SIGNAL PROCESSING
+CREATED: 04 OCT 2016
+MODIFIY: 04 OCT 2016
+
+DESCRIPTION: Plot processed signal of sonic boom.
+narrow-band spectrum
+single-side spectral density
+SPL
+octave bands
+overall SPL
+"""
+
+import numpy as np
+import os
+
+#CUSTOM PLOTTING PACKAGE
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/Users/Logan/lib/python')
+from lplot import *
+
+
+def main():
+    """input description
+    """
+
+
+if __name__ == "__main__":
+    main()
